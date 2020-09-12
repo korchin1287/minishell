8d923037561bb029715901763dbe454a07ae964d
Kanat Saribaew <ndreadno@mi-i3.msk.21-school.ru>
2020-09-12 16:44:09 +0300

    "2"

--- Makefile
+++ Makefile
@@ -3,16 +3,16 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: nofloren <marvin@42.fr>                    +#+  +:+       +#+         #
+#    By: ndreadno <ndreadno@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/05/12 18:43:36 by ndreadno          #+#    #+#              #
-#    Updated: 2020/09/10 16:03:40 by nofloren         ###   ########.fr        #
+#    Updated: 2020/09/12 13:19:56 by ndreadno         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = minishell
 SRCS = srcs/minishell.c srcs/parser.c srcs/ft_pork.c
-OBJ  =srcs/minishell.o srcs/parser.o srcs/ft_pork.o
+OBJ  = srcs/minishell.o srcs/parser.o srcs/ft_pork.o
 LIB = libft/libft.a
 INCLUDES = ./srcs
 FLAGS = -g 
@@ -20,10 +20,14 @@ all: $(NAME)
 
 $(NAME):$(OBJ) $(LIB)
 	gcc $(FLAGS) $(OBJ) $(LIB) -o $(NAME)
+	/bin/rm -f test
 
 %.o:%.c
 	gcc $(FLAGS) -I $(INCLUDES) -c $< -o $@
 
+ts:
+	gcc -g srcs/*.c libft/*.c -o test
+	/bin/rm -f minishell
 $(LIB):
 	@$(MAKE) -C libft/
 
@@ -34,7 +38,4 @@ clean:
 fclean:clean
 	/bin/rm -f $(NAME)
 	/bin/rm -f $(LIB)
-
 re:fclean all
-
-.PHONY: re, clean, fclean, all
--- srcs/parser.c
+++ srcs/parser.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nofloren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ndreadno <ndreadno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 12:47:52 by ndreadno          #+#    #+#             */
-/*   Updated: 2020/09/10 15:21:17 by nofloren         ###   ########.fr       */
+/*   Updated: 2020/09/12 16:37:01 by ndreadno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char		**make_map(t_list **map, int size)
+char		**make_map(t_tmp_list **map, int size)
 {
 	char	**str;
 	int		i;
-	t_list	*tmp;
+	t_tmp_list	*tmp;
 
 	if (!(str = malloc(sizeof(char *) * (size + 1))))
 		exit(-1);
@@ -32,9 +32,9 @@ char		**make_map(t_list **map, int size)
 	return (str);
 }
 
-void ft_clear_list(t_list **head)
+void ft_clear_list(t_tmp_list **head)
 {
-	t_list *tmp;
+	t_tmp_list *tmp;
 
 	
 	while (*head)
@@ -48,11 +48,11 @@ void ft_clear_list(t_list **head)
 		
 	}
 }
-t_list	*ft_add(t_data *data, char *str)
+t_tmp_list	*ft_add(t_data *data, char *str)
 {
-	t_list *new;
+	t_tmp_list *new;
 
-	new = (t_list *)malloc(sizeof(t_list));
+	new = (t_tmp_list *)malloc(sizeof(t_tmp_list));
 	if (new == NULL)
 	{
 		// vars->parse.erorrs.flag_malloc_error = 1;
@@ -63,9 +63,9 @@ t_list	*ft_add(t_data *data, char *str)
 	return (new);
 }
 
-void		ft_add_end(t_list **start, t_list *new)
+void		ft_add_end(t_tmp_list **start, t_tmp_list *new)
 {
-	t_list *tmp;
+	t_tmp_list *tmp;
 
 	tmp = *start;
 	if (tmp == NULL)
@@ -77,9 +77,9 @@ void		ft_add_end(t_list **start, t_list *new)
 		tmp = tmp->next;
 	tmp->next = new;
 }
-int ft_size_list(t_list *start)
+int ft_size_list(t_tmp_list *start)
 {
-	t_list	*tmp;
+	t_tmp_list	*tmp;
 	int		i;
 
 	i = 0;
@@ -105,11 +105,150 @@ void ft_init(t_data *data)
 	data->parser.flag_close_quotes = 0;
 	data->parser.flag_open_quotes = 0;
 	data->parser.flag_single_quotes = 0;
+	data->parser.flag_single_quotes_count = 0;
 	data->parser.flag_double_quotes = 0;
+	data->parser.flag_double_quotes_count = 0;
 	data->echo.flag_n = 0;
 	data->arg_list = NULL;
 	data->parser.count_arg = 0;
 	data->echo_flag = 0;
+}
+
+void ft_check_open_qoutes(t_data *data, char *str, int *i)
+{
+	int k = *i;
+	while (str[*i])
+	{
+		if (str[*i] == '\"' && !data->parser.flag_single_quotes)
+		{
+			if(*i != 0 && str[*i - 1] == '\\')
+			{
+				continue;
+				(*i)++;
+			}
+			if (!data->parser.flag_double_quotes)
+				k = *i + 1;
+			data->parser.flag_double_quotes = data->parser.flag_double_quotes ? 0 : 1;
+			data->parser.flag_double_quotes_count++;
+		}
+		if (str[*i] == '\'' && !data->parser.flag_double_quotes)
+		{
+			if(*i != 0 && str[*i - 1] == '\\')
+			{
+				continue;
+				(*i)++;
+			}
+			if(!data->parser.flag_single_quotes)
+				k = *i + 1;
+			data->parser.flag_single_quotes = data->parser.flag_single_quotes ? 0 : 1;
+			data->parser.flag_single_quotes_count++;
+		}
+		(*i)++;
+	}
+	*i = k;
+}
+
+char  *ft_check_close_qoutes(t_data *data, char *str, int *i)
+{
+	char *s;
+	int k;
+	char c = str[*i];
+	char q;
+
+	k = *i;
+	while (str[*i] != '\0')
+	{
+		if (str[*i] == '\'' && data->parser.flag_single_quotes == 1)
+		{
+			if (str[*i - 1] == '\\')
+			{
+				(*i)++;
+				continue;
+			}
+			if (data->parser.flag_single_quotes_count % 2 == 0)
+			{
+				data->parser.flag_close_quotes = 1;
+				(*i)++;
+			}
+		}
+		if (str[*i] == '\"' && data->parser.flag_double_quotes == 1)
+		{
+			if (str[*i - 1] == '\\')
+			{
+				(*i)++;
+				continue;
+			}
+			if (data->parser.flag_double_quotes_count % 2 == 0)
+			{
+				data->parser.flag_close_quotes = 1;
+				(*i)++;
+			}
+		}
+		(*i)++;
+	}
+	if (data->parser.flag_close_quotes != 1)
+	{
+		*i = k;
+		char *line;
+		if (data->parser.flag_single_quotes == 1)
+		{
+			
+			write(1, "quote> ", ft_strlen("quote> "));
+			ft_get_next_line(0, &line);
+			str = ft_strjoin(str, "\\n");
+			str = ft_strjoin(str, line);
+			ft_check_open_qoutes(data, str, i);
+			return (ft_check_close_qoutes(data, str, i));
+		}
+		if (data->parser.flag_double_quotes == 1)
+		{
+			write(1, "dquote> ", ft_strlen("dquote> "));
+			ft_get_next_line(0, &line);
+			str = ft_strjoin(str, "\\n");
+			str = ft_strjoin(str, line);
+			ft_check_open_qoutes(data, str, i);
+			return (ft_check_close_qoutes(data, str, i));
+		}
+	}
+	return (str);
+}
+
+// char *trim_qoutes(t_data *data, char *str)
+// {
+// 	int i;
+
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		if (data->parser.flag_single_quotes == 1 && str[i] == '\'')
+// 		{}
+// 	}
+// }
+
+char *ft_string(t_data *data, char *str, int *i)
+{
+	int k;
+	char *s;
+
+	k = *i;
+	while (str[*i])
+	{
+		if (str[*i] == ' ')
+			break;
+		if (str[*i] == ';' || str[*i] == '|')
+		{
+			data->parser.flag_arg = str[*i];
+			data->parser.arg = 1;
+			break;
+		}	
+		(*i)++;
+	}
+	s = ft_substr(str, k, *i - k);
+	if (data->parser.flag_open_quotes != 1)
+		s = ft_strtrim_char(s, '\'');
+	*i = ft_space(str, *i);
+	return s;
+	
 }
 
 char *ft_qoutes(t_data *data, char *str, int *i)
@@ -124,81 +263,96 @@ char *ft_qoutes(t_data *data, char *str, int *i)
 	{
 		if (str[*i] == '\'' && data->parser.flag_single_quotes == 1)
 		{
-			data->parser.flag_close_quotes = 1;
+			if (str[*i - 1] == '\\')
+			{
+				(*i)++;
+				continue;
+			}
+			if(str[*i + 1] == '\0' || str[*i + 1] == ' ')
+				data->parser.flag_close_quotes = 1;
 			break;
 		}
 		if (str[*i] == '\"' && data->parser.flag_double_quotes == 1)
 		{
-			data->parser.flag_close_quotes = 1;
+			if (str[*i - 1] == '\\')
+			{
+				(*i)++;
+				continue;
+			}
+			if(str[*i + 1] == '\0' || str[*i + 1] == ' ')
+				data->parser.flag_close_quotes = 1;
 			break;
 		}
 		(*i)++;
 	}
 	if (data->parser.flag_close_quotes != 1)
-		return NULL;
+	{
+		*i = k;
+		return (NULL);
+	}
 	q = str[*i];
 	s = ft_substr(str, k , *i - k);
-	*i = ft_space(str, *i + 1);
+	if (str[*i] == '\"' || str[*i] == '\'' )
+		(*i)++;
+	*i = ft_space(str, *i);
 	return (s);
 }
-char *ft_string(t_data *data, char *str, int *i)
-{
-	int k;
-	char *s;
 
-	k = *i;
-	while (str[*i])
-	{
-		if (str[*i] == ' ')
-			break;	
-		(*i)++;
-	}
-	s = ft_substr(str, k, *i - k);
-	//s = ft_strtrim_new(s, "\"'");
-	*i = ft_space(str, *i);
-	return s;
-	
-}
-char **ft_parse_line(char *line)
+char **ft_parse_line(t_list_arg *list, char *line)
 {
 	char *str;
+	char *tmp;
 	char **s;
 	int i;
 	t_data data;
-
 	ft_init(&data);
 	i = 0;
 	str = ft_strtrim(line, " \t");
+	ft_check_open_qoutes(&data, str, &i);
+	str = ft_check_close_qoutes(&data, str, &i);
+	ft_quotes_null(&data);
+	i = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] == '\'')
 		{
-			data.parser.flag_single_quotes = 1;
+			if (i == 0 || str[i - 1] == ' ')
+				data.parser.flag_single_quotes = 1;
 		}
 		if (str[i] == '\"')
 		{
-			data.parser.flag_double_quotes = 1;
+			if (i == 0 || str[i - 1] == ' ')
+				data.parser.flag_double_quotes = 1;
 		}
 		if (data.parser.flag_single_quotes != 1 && data.parser.flag_double_quotes != 1)
 		{
-			ft_add_end(&data.arg_list, ft_add(&data, ft_string(&data, str, &i)));
+			if ((tmp = ft_string(&data, str, &i)))
+				ft_add_end(&data.arg_list, ft_add(&data, tmp));
 		}
-		if (data.parser.flag_single_quotes == 1 ||data.parser.flag_double_quotes == 1)
+		if (data.parser.flag_single_quotes == 1 || data.parser.flag_double_quotes == 1)
 		{
+			data.parser.flag_open_quotes = 1;
 			i++;
-			ft_add_end(&data.arg_list, ft_add(&data, ft_qoutes(&data, str, &i)));
+			if ((tmp = ft_qoutes(&data, str, &i)))
+				ft_add_end(&data.arg_list, ft_add(&data, tmp));
 			ft_quotes_null(&data);
 		}
+		// if (data.parser.arg == 1)
+		// {
+		// 	ft_lstadd_back(&list, ft_lstnew());
+		// }
+		
 	}
 	data.parser.count_arg = ft_size_list(data.arg_list);
 	s = make_map(&data.arg_list, data.parser.count_arg);
-	ft_clear_list(&data.arg_list);
-	return (s);
-	// while (data->arg_list)
-	// {
-	// 	printf("%s\n", data->arg_list->str);
-	// 	data->arg_list = data->arg_list->next;
-	// }
+	
 	// exit(0);
-	// ft_check_command(data, &data->arg_list);
+	
+	// while (data.arg_list)
+	// {
+	// 	printf("%s\n", data.arg_list->str);
+	// 	data.arg_list = data.arg_list->next;
+	// }
+	//ft_clear_list(&data.arg_list);
+	return (s);
 }
