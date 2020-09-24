6b5383605c2ab8aecb4e2fe88a9faf3bc23cfb08
Kanat Saribaew <ndreadno@am-l1.msk.21-school.ru>
2020-09-24 13:07:49 +0300

    исправил мелкие косяки в unset export и ft_command_bash

--- srcs/minishell/ft_pork.c
+++ srcs/minishell/ft_pork.c
@@ -6,7 +6,7 @@
 /*   By: ndreadno <ndreadno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 17:01:56 by nofloren          #+#    #+#             */
-/*   Updated: 2020/09/21 23:13:32 by ndreadno         ###   ########.fr       */
+/*   Updated: 2020/09/22 14:36:57 by ndreadno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,12 @@ int		ft_pork(t_shell *shell, char *path, char **env)
 
 	int savefd;
 	int oldfd;
-	// if (shell->list_arg->flag_pipe == 1)
-	// {
-	// 	pipe(shell->fd1);
-	// 	if (shell->list_arg->back && shell->list_arg->back->flag_pipe == 1)
-	// 		pipe(shell->fd2);
-	// }
+	if (shell->list_arg->flag_pipe == 1)
+	{
+		pipe(shell->fd1);
+		if (shell->list_arg->back && shell->list_arg->back->flag_pipe == 1)
+			pipe(shell->fd2);
+	}
 	// savefd = open("test.txt", O_CREAT | O_RDWR, 0666);
 	pid = fork();
 	process = pid;
@@ -50,23 +50,28 @@ int		ft_pork(t_shell *shell, char *path, char **env)
 	s2 = ft_strjoin(s2, shell->list_arg->arg[shell->j]);
 	if (pid == 0)
 	{
-		// if (shell->list_arg->flag_pipe == 1)
-		// {
-		// 	dup2(shell->fd1[1], 1);
-		// 	close(shell->fd1[0]);
-		// }
-		// if (shell->list_arg->flag_pipe == 0 && !shell->list_arg->back)
-		// {
-		// 	dup2(shell->fd1[0], 0);
-		// 	close(shell->fd1[1]);
-		// }
-		// if (shell->list_arg->flag_pipe == 1 && shell->list_arg->back && shell->list_arg->back->flag_pipe == 1)
-		// {
-		// 	dup2(shell->fd1[0], 0);
-		// 	close(shell->fd1[1]);
-		// 	dup2(shell->fd2[1], 1);
-		// 	close(shell->fd2[0]);
-		// }
+		if (shell->list_arg->flag_pipe == 1)
+		{
+			dup2(shell->fd1[1], 1);
+			//close(shell->fd1[0]);
+		}
+		if (shell->list_arg->flag_pipe == 0 /*&& !shell->list_arg->back*/)
+		{
+			dup2(shell->fd1[0], 0);
+			//close(shell->fd1[1]);
+		}
+		if (shell->list_arg->flag_pipe == 0  &&  shell->list_arg->back && shell->list_arg->back->flag_pipe == 1/*&& !shell->list_arg->back*/)
+		{
+			dup2(shell->fd2[0], 0);
+			//close(shell->fd1[1]);
+		}
+		if (shell->list_arg->flag_pipe == 1 && shell->list_arg->back && shell->list_arg->back->flag_pipe == 1)
+		{
+			dup2(shell->fd1[0], 0);
+			//close(shell->fd1[1]);
+			dup2(shell->fd2[1], 1);
+			//close(shell->fd2[0]);
+		}
 		
 		
 		if (execve(s2, &shell->list_arg->arg[shell->j], env) == -1)
@@ -75,13 +80,43 @@ int		ft_pork(t_shell *shell, char *path, char **env)
 			shell->flag_exit = WEXITSTATUS(status);
 			exit (WEXITSTATUS(status));
 		}
+		if (shell->list_arg->flag_pipe)
+			close(shell->fd1[1]);
+		if (shell->list_arg->flag_pipe == 0 /*&& !shell->list_arg->back*/)
+		{
+			close(shell->fd1[0]);
+		}
+		if (shell->list_arg->flag_pipe == 1 && shell->list_arg->back && shell->list_arg->back->flag_pipe == 1)
+		{
+			close(shell->fd1[0]);
+			//close(shell->fd1[1]);
+			close(shell->fd2[1]);
+			//close(shell->fd2[0]);
+		}
+		
+		if (shell->list_arg->flag_pipe == 0  &&  shell->list_arg->back && shell->list_arg->back->flag_pipe == 1/*&& !shell->list_arg->back*/)
+		{
+			close(shell->fd2[0]);
+		}
 	}
 	else if (pid < 0)
 		perror("lsh");
 	else
 	{
-	//	close(shell->fd1[0]);
-		//close(shell->fd1[1]);
+		//close(shell->fd1[0]);
+		if (shell->list_arg->flag_pipe)
+			close(shell->fd1[1]);
+		if (shell->list_arg->flag_pipe == 0 /*&& !shell->list_arg->back*/)
+			close(shell->fd1[0]);
+		if (shell->list_arg->flag_pipe == 1 && shell->list_arg->back && shell->list_arg->back->flag_pipe == 1)
+		{
+			close(shell->fd1[0]);
+			close(shell->fd2[1]);
+		}
+		if (shell->list_arg->flag_pipe == 0  &&  shell->list_arg->back && shell->list_arg->back->flag_pipe == 1/*&& !shell->list_arg->back*/)
+		{
+			close(shell->fd2[0]);
+		}
 		wpid = waitpid(pid, &status, WUNTRACED);
 	}
 	return (WEXITSTATUS(status));
